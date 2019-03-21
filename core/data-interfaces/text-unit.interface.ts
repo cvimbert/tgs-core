38f2fe1d9b77e33ca5d2c0ddd32587cd8c618877
Christophe <ch.vimbert@gmail.com>
2019-03-21 18:25:19 +0100

    Tags

--- core/data-interfaces/text-unit.interface.ts
+++ core/data-interfaces/text-unit.interface.ts
@@ -1,5 +1,8 @@
+import { TagModel } from "tgs-model";
+
 export interface TextUnit {
   text?: string;
   units?: TextUnit[];
   styles?: string[];
+  tag?: TagModel;
 }
--- core/game-sequence.class.ts
+++ core/game-sequence.class.ts
@@ -32,28 +32,26 @@ export class GameSequence {
   }
 
   init() {
-    this.loadBlock(this.structureData.entryBlockId);
-
-    // on devra peut-être attrendre que le contexte soit correctement initialisé
-    if (this.scripts["start"]) {
-      this.scripts["start"].execute();
+    // on devra peut-être attendre que le contexte soit correctement initialisé
+    if (this.scripts["init"]) {
+      this.scripts["init"].execute();
     }
+
+    this.loadBlock(this.structureData.entryBlockId);
   }
 
   loadBlock(blockId: string) {
 
     let block: GameBlockModel = this.structureData.blocks[blockId];
-
-    let lines: string[] = this.getBlockTextLines(block.lines);
-    //console.log("lines", lines);
-
-    let paragraphs: string[] = this.stringsToParagraphs(lines);
-    this.sequence.paragraphs.push(paragraphs);
-    //console.log("paragraphs", this.sequence.paragraphs);
-
     let links: LinkModel[] = this.getBlockLinks(block.links);
 
     this.units.push(this.getTextUnits(block.lines));
+
+    // execution des scripts locaux
+    if (block.scripts && block.scripts["start"]) {
+      let script: Script = new Script(block.scripts["start"]);
+      script.execute();
+    }
 
     // if pas utile
     if (links) {
@@ -87,52 +85,15 @@ export class GameSequence {
           }
 
           break;
+
+        case BlockLineType.TAG:
+          unit.tag = line.tag;
+          units.push(unit);
+          break;
       }
     });
 
     return units;
-  }
-
-  getBlockTextLines(lines: GameBlockLineModel[]): string[] {
-    let texts: string[] = [];
-
-    lines.forEach(line => {
-      switch(line.type) {
-        case BlockLineType.SIMPLE:
-          texts.push(line.text);
-          break;
-
-        case BlockLineType.COMPLEX:
-          if (this.evaluateCondition(line.condition)) {
-            texts.push(...this.getBlockTextLines(line.lines));
-          }
-
-          break;
-      }
-    });
-
-    return texts;
-  }
-
-  // plus utile
-  getStyleStack(): string[] {
-    return this.styleStack.slice();
-  }
-
-  // plus utile
-  getClasses(): string {
-
-    let styles = "";
-
-    this.styleStack.forEach((styleName: string, index: number) => {
-      styles += styleName;
-
-      if (index < this.styleStack.length - 1) {
-        styles += "";
-      }
-    });
-
-    return styles;
   }
 
   evaluateCondition(model: ConditionModel): boolean {
@@ -151,37 +112,5 @@ export class GameSequence {
     }
 
     return links;
-  }
-
-  addString(added: string, addedTo: string): string {
-    return (addedTo === "") ? added : (addedTo + " " + added);
-  }
-
-  stringsToParagraphs(texts: string[]): string[] {
-    let paragraphs: string[] = [];
-    let currentLine = "";
-
-    for (let i: number = 0; i < texts.length; i++) {
-      if (i < texts.length - 1) {
-        if (texts[i + 1] !== this.PARAGRAPH_SEPARATOR) {
-          currentLine = this.addString(texts[i], currentLine);
-        } else if (texts[i + 1] === this.PARAGRAPH_SEPARATOR)  {
-          paragraphs.push(this.addString(texts[i], currentLine));
-          currentLine = "";
-          i++;
-        }
-      }
-      else {
-        if (texts[i] != this.PARAGRAPH_SEPARATOR) {
-          currentLine = this.addString(texts[i], currentLine);
-        }
-      }
-    }
-
-    if (currentLine !== "") {
-      paragraphs.push(currentLine);
-    }
-
-    return paragraphs;
   }
 }
